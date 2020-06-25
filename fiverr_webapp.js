77da2d2eac8aab526bc177d014c90ce559745a2f
Yuval Podoksik <42583587+yuvalp29@users.noreply.github.com>
2020-06-25 20:22:32 +0300

    Update fiverr_webapp.js

--- fiverr_webapp.js
+++ fiverr_webapp.js
@@ -1,8 +1,10 @@
 const port = 80
+var os = require("os")
+
 require('http')
   .createServer((req, res) => {
     console.log('url:', req.url)
-    res.end('hello world')
+    res.end(os.hostname())
   })
   .listen(port, (error)=>{
     console.log(`server is running on ${port}`)
