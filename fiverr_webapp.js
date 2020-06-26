3f966bb7710809e4b4549a64e13be51acbecedb0
Yuval Podoksik <42583587+yuvalp29@users.noreply.github.com>
2020-06-26 21:41:18 +0300

    Update fiverr_webapp.js

--- fiverr_webapp.js
+++ fiverr_webapp.js
@@ -1,11 +1,29 @@
+// Variables and Constants
 const port = 80
+var request = require('request');
+var http = require('http');
+var ip = require("ip");
 var os = require("os")
+var dns = '';
+var publicIP = '';
 
-require('http')
-  .createServer((req, res) => {
-    console.log('url:', req.url)
-    res.end(os.hostname())
-  })
-  .listen(port, (error)=>{
-    console.log(`server is running on ${port}`)
-  })
+request('http://169.254.169.254/latest/meta-data/public-hostname', function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+       dns = body;
+   }
+})
+
+request('http://169.254.169.254/latest/meta-data/public-ipv4', function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+       publicIP = body;
+   }
+})
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Hostname: ' + os.hostname() + '<br/>');
+  res.write('DNS: ' + `${dns}` + '<br/>');
+  res.write('Public IP: ' + `${publicIP}` + '<br/>');
+  res.write('Private IP: ' + ip.address() + '<br/>');
+  res.end();
+}).listen(port, (error)=>{console.log(`server is running on ${port}`)})
